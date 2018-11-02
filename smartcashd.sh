1065c3ab1b8d21369d8c68e8343a8a9159d3d7f2
mikeytown2 <mike.carper@gmail.com>
2018-11-01 17:10:52 -0700

    Update smartcashd.sh

--- smartcashd.sh
+++ smartcashd.sh
@@ -48,7 +48,7 @@ USE_DROPBOX_BOOTSTRAP=1
 DROPBOX_BLOCKS_N_CHAINS='scjugxv8kco8z86'
 
 # Multiple on single IP.
-MULTI_IP_MODE=1
+MULTI_IP_MODE=2
 # Mini Monitor check masternode list.
 MINI_MONITOR_MN_LIST=0
 # Mini Monitor Status to check for.
