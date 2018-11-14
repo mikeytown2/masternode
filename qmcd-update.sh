0d47534f449cb618159f69ef9f2cf634cff9c393
mikeytown2 <mike.carper@gmail.com>
2018-11-14 01:07:03 -0800

    Update qmcd-update.sh

--- qmcd-update.sh
+++ qmcd-update.sh
@@ -96,7 +96,7 @@ done
 # shellcheck source=/root/___mn.sh
 . ~/___mn.sh
 . ~/.bashrc
-UPDATE_DAEMON_ADD_CRON "${BIN_BASE}" "${GITHUB_REPO}" "${CONF_FILE}" "${DAEMON_DOWNLOAD}"
+UPDATE_DAEMON_ADD_CRON "${BIN_BASE}" "${GITHUB_REPO}" "${CONF_FILE}" "${DAEMON_DOWNLOAD}" "${DIRECTORY}"
 # shellcheck source=/root/.bashrc
 . ~/.bashrc
 stty sane
