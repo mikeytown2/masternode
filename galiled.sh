dd4d8c6cf560a455d9a9e43ce5b598be611381e7
Mike Carper <mike.carper@gmail.com>
2018-11-13 19:49:35 -0500

    Update galilel for GA.

--- galiled.sh
+++ galiled.sh
@@ -8,84 +8,48 @@
 # Run this file
 # bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/galiled.sh)" ; source ~/.bashrc
 
-# Directory
-DIRECTORY='.galilel'
-# Port
-DEFAULT_PORT=36001
-# Conf File
-CONF='galilel.conf'
-# Display Name
-DAEMON_NAME='Galilel'
 # Github user and project.
 GITHUB_REPO='Galilel-Project/galilel'
+# Display Name.
+DAEMON_NAME='Galilel Core'
+# Coin Ticker.
+TICKER='GALI'
 # Binary base name.
 BIN_BASE='galilel'
-# Direct Daemon Download if github has no releases.
-DAEMON_DOWNLOAD=''
-# Username Prefix
-DAEMON_PREFIX='gali_mn'
-# RPC username
-RPC_USERNAME='galilel'
+# Directory.
+DIRECTORY='.galilel'
+# Conf File.
+CONF='galilel.conf'
+# Port.
+DEFAULT_PORT=36001
 # Explorer URL
 EXPLORER_URL='https://explorer.galilel.cloud/'
-# Log filename
-DAEMON_SETUP_LOG='/tmp/galilel.log'
-# Masternode output file.
-DAEMON_SETUP_INFO="${HOME}/galilel.mn.txt"
-# Project Folder
 # Amount of Collateral needed
 COLLATERAL=15000
-# Coin Ticker
-TICKER='GALI'
-# Tip Address
-TIPS=''
-# Dropbox Addnodes
-DROPBOX_ADDNODES=''
-# If set to 1 then use addnodes from dropbox.
-USE_DROPBOX_ADDNODES=0
-# Dropbox Bootstrap
-DROPBOX_BOOTSTRAP=''
-# If set to 1 then use bootstrap from dropbox.
-USE_DROPBOX_BOOTSTRAP=0
-# Dropbox blocks and chainstake folders.
-DROPBOX_BLOCKS_N_CHAINS=''
-# Cycle Daemon
-DAEMON_CYCLE=0
 # Fallback Blockcount
 BLOCKCOUNT_FALLBACK_VALUE=223000
-# Slow Daemon Start.
-SLOW_DAEMON_START=0
-# Bad Explorer SSL.
-BAD_SSL_HACK=''
-# Extra configuation for the conf file.
-EXTRA_CONFIG=''
-# Auto Recovery.
-RESTART_IN_SYNC=1
 # Multiple on single IP.
 MULTI_IP_MODE=1
-# Number of Connections to wait for.
-DAEMON_CONNECTIONS=6
-# Wait for MNSYNC
-#MNSYNC_WAIT_FOR='"RequestedMasternodeAssets": 999,'
-MNSYNC_WAIT_FOR=''
-# Run Mini Monitor.
-MINI_MONITOR_RUN=1
+
+# Tip Address
+TIPS='UYFEvLMguJaLDXAZPtynAPKWC3hWh6vXKY'
+# Dropbox Addnodes
+DROPBOX_ADDNODES='g42lx19ifp6zo6k'
+# If set to 1 then use addnodes from dropbox.
+USE_DROPBOX_ADDNODES=1
+# Dropbox Bootstrap
+DROPBOX_BOOTSTRAP='ro3oo9rjyf8o8kb'
+# If set to 1 then use bootstrap from dropbox.
+USE_DROPBOX_BOOTSTRAP=1
+# Dropbox blocks and chainstake folders.
+DROPBOX_BLOCKS_N_CHAINS='pjmfe78kjdprh49'
+
 # Mini Monitor check masternode list.
-MINI_MONITOR_MN_LIST=0
+MINI_MONITOR_MN_LIST=1
 # Mini Monitor Status to check for.
-MINI_MONITOR_MN_STATUS='Masternode successfully started'
-# Mini Monitor Queue Payouts.
-MINI_MONITOR_MN_QUEUE=1
+MINI_MONITOR_MN_STATUS='4'
 # Mini Monitor masternode count is a json string.
-MINI_MONITOR_MN_COUNT_JSON=0
-
-# Log to a file.
-rm -f "${DAEMON_SETUP_LOG}"
-touch "${DAEMON_SETUP_LOG}"
-chmod 600 "${DAEMON_SETUP_LOG}"
-exec >  >(tee -ia "${DAEMON_SETUP_LOG}")
-exec 2> >(tee -ia "${DAEMON_SETUP_LOG}" >&2)
-
+MINI_MONITOR_MN_COUNT_JSON=1
 
 ASCII_ART () {
 echo -e "\\e[0m"
@@ -99,10 +63,6 @@ cat << "GALILEL"
 \____/\__,_|_|_|_|\___|_|
 
 GALILEL
-}
-
-SENTINEL_SETUP () {
-  echo
 }
 
 # Discord User Info
