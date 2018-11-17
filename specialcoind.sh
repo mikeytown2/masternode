1f08e8b5770c18a3929f641f3a24d76cd9501e9e
mikeytown2 <mike.carper@gmail.com>
2018-11-17 07:08:24 -0800

    Update specialcoind.sh

--- specialcoind.sh
+++ specialcoind.sh
@@ -27,7 +27,7 @@ EXPLORER_URL='http://192.241.138.224:3001/'
 # Amount of Collateral needed.
 COLLATERAL=1000
 # Number of Connections to wait for.
-DAEMON_CONNECTIONS=1
+DAEMON_CONNECTIONS=2
 # Cycle Daemon on first start.
 DAEMON_CYCLE=1
 
