2ac1bcfb94d62e905908b176aed14bf8a14f75e9
mikeytown2 <mike.carper@gmail.com>
2018-11-19 18:55:11 -0800

    Update blocknetdxd.sh

--- blocknetdxd.sh
+++ blocknetdxd.sh
@@ -28,6 +28,8 @@ EXPLORER_URL='https://block.ccore.online/'
 COLLATERAL=5000
 # Cycle Daemon on first start.
 DAEMON_CYCLE=1
+MASTERNODE_CALLER='servicenode'
+MASTERNODE_PREFIX='sn'
 
 # Tip Address
 TIPS=''
