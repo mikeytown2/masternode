3922a0276f625124e6e0ca6c7f919c9fb67f0813
mikeytown2 <mike.carper@gmail.com>
2018-12-18 19:07:36 -0800

    Update zcoind.sh

--- nocollateral/zcoind.sh
+++ nocollateral/zcoind.sh
@@ -9,7 +9,7 @@
 # Run this file
 
 ```
-bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/zcoind.sh)" ; source ~/.bashrc
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/nocollateral/zcoind.sh)" ; source ~/.bashrc
 ```
 
 '
