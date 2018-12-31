9546f8c65a9ca08daa5ef4d7645796acf0c8dcee
mikeytown2 <mike.carper@gmail.com>
2018-12-30 17:53:09 -0800

    Update and rename nocollateral/qbicd.sh to qbicd.sh

--- qbicd.sh
+++ qbicd.sh
@@ -9,7 +9,7 @@
 # Run this file
 
 ```
-bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/nocollateral/qbicd.sh)" ; source ~/.bashrc
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/qbicd.sh)" ; source ~/.bashrc
 ```
 
 '
