6279afbbb3a53eca44e923b2f1c37bac1e44966a
mikeytown2 <root@github.net>
2019-01-14 19:48:10 +0100

    coin unlocked

--- armageddond.sh
+++ armageddond.sh
@@ -9,7 +9,7 @@
 # Run this file
 
 ```
-bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/nocollateral/armageddond.sh)" ; source ~/.bashrc
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/armageddond.sh)" ; source ~/.bashrc
 ```
 
 '
