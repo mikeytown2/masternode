6552c7b81116da1dfde302a215821b3769e58c0f
mikeytown2 <mike.carper@gmail.com>
2019-02-06 16:06:29 -0800

    Update millenniumclubd.sh

--- millenniumclubd.sh
+++ millenniumclubd.sh
@@ -9,7 +9,7 @@
 # Run this file
 
 ```
-bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/quantisnetd.sh)" ; source ~/.bashrc
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/millenniumclubd.sh)" ; source ~/.bashrc
 ```
 
 '
