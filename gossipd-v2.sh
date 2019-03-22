447470c985a054eb51b709851a29658c65efa708
mikeytown2 <mike.carper@gmail.com>
2019-03-21 18:59:09 -0700

    Update gossipd-v2.sh

--- gossipd-v2.sh
+++ gossipd-v2.sh
@@ -9,7 +9,7 @@
 # Run this file
 
 ```
-bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/gossipd.sh)" ; source ~/.bashrc
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/gossipd-v2.sh)" ; source ~/.bashrc
 ```
 
 '
