295ef5f62150b9e3ce47af2217cbde689f21678b
mikeytown2 <mike.carper@gmail.com>
2019-05-28 13:44:34 -0700

    Update stakecubed-update.sh

--- stakecubed-update.sh
+++ stakecubed-update.sh
@@ -9,7 +9,7 @@
 # Run this file
 
 ```
-bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/stakecubed.sh)" ; source ~/.bashrc
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/mikeytown2/masternode/master/stakecubed-update.sh)" ; source ~/.bashrc
 ```
 
 '
