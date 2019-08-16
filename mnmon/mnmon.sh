bdc3ac4f06719d34d623c1a63bcd9f2471b98574
mikeytown2 <mikeytown2@github.com>
2019-08-17 01:07:42 +0200

    update mnmon

--- mnmon/mnmon.sh
+++ mnmon/mnmon.sh
@@ -1300,12 +1300,13 @@ in approximately ${MN_REWARD_IN_TIME}." "" "" "${DISCORD_WEBHOOK_USERNAME}" "${D
   fi
 
   # Report on uptime
-  UPTIME
   PAST_UPTIME=$( SQL_QUERY "SELECT value FROM variables WHERE key = '${CONF_LOCATION}:uptime';" )
   if [[ -z "${PAST_UPTIME}" ]]
   then
     PAST_UPTIME="${UPTIME}"
   fi
+  echo "uptime: ${UPTIME}
+past uptime: ${PAST_UPTIME}"
   if [[ "${UPTIME}" -lt "${PAST_UPTIME}" ]]
   then
     PAST_UPTIME_HUMAN=$( DISPLAYTIME "${PAST_UPTIME}" )
@@ -1314,12 +1315,12 @@ in approximately ${MN_REWARD_IN_TIME}." "" "" "${DISCORD_WEBHOOK_USERNAME}" "${D
     then
       SEND_ERROR "__${USRNAME} ${DAEMON_BIN}__
 Daemon was restarted mutiple times in teh last 5 minutes.
-Past uptime: ${PAST_UPTIME_HUMAN}.
+Past uptime: ${PAST_UPTIME_HUMAN}
 New uptime: ${UPTIME_HUMAN} " "" "${DISCORD_WEBHOOK_USERNAME}" "${DISCORD_WEBHOOK_AVATAR}"
     else
       SEND_WARNING "__${USRNAME} ${DAEMON_BIN}__
 Daemon was restarted.
-Past uptime: ${PAST_UPTIME_HUMAN}.
+Past uptime: ${PAST_UPTIME_HUMAN}
 New uptime: ${UPTIME_HUMAN}" "" "${DISCORD_WEBHOOK_USERNAME}" "${DISCORD_WEBHOOK_AVATAR}"
     fi
   fi
